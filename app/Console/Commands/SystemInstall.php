a0867a658a53669acd11958b50d311a06dbf5f99
Lorraine-Olegario <lorraineolegario16@gamil.com>
2024-08-03 19:03:08 -0300

    feat: installation file for the project

---
+++ app/Console/Commands/SystemInstall.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+
+class SystemInstall extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'app:install';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Command description';
+
+    /**
+     * Execute the console command.
+     */
+    public function handle()
+    {
+        try {
+            $this->call('key:generate');
+            $this->call('migrate');
+            $this->call('db:seed');
+            $this->call('config:cache');
+            $this->call('event:cache');
+            $this->call('route:cache');
+            $this->call('view:cache');
+
+            $this->info('System installed successfully.');
+        } catch (\Exception $e) {
+            $this->error($e->getMessage());
+        }
+
+    }
+}
--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -7,6 +7,11 @@ use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
 
 class Kernel extends ConsoleKernel
 {
+
+    protected $commands = [
+        \App\Console\Commands\SystemInstall::class,
+    ];
+
     /**
      * Define the application's command schedule.
      */
@@ -24,4 +29,5 @@ class Kernel extends ConsoleKernel
 
         require base_path('routes/console.php');
     }
+
 }
