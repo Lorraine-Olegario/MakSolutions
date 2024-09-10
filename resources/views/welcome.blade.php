1229ae65ccae42dfab3c0a5bdb045abbbd780f16
Lorraine-Olegario <lorraineolegario16@gamil.com>
2024-09-10 13:05:17 -0300

    update

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -166,7 +166,7 @@
                         <div class="row mt-4">
                             <div class="col text-center">
                                 <a href="https://api.whatsapp.com/send?phone={{ $company->whatsapp_suporte }}&text=Olá, preciso falar com o comercial."
-                                    class="btn btn-outline-light btn-lg rounded-pill px-5">
+                                    class="btn btn-outline-light btn-lg rounded-pill px-5" target="_blank">
                                     Solicitar Orçamento!
                                 </a>
                             </div>
